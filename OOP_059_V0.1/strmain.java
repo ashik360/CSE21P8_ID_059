4505ab5bfab95107092413425e2246e1aa287749
ashik360 <63978080+ashik360@users.noreply.github.com>
2022-06-24 11:54:40 +0600

    Add files via upload
    StrOp

---
+++ OOP_059_V0.1/strmain.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class StrMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Scanner s = new Scanner(System.in);
+		StringOperation operation = new StringOperation();
+		
+//		System.out.println("Enter a string to start the operation: ");
+		
+//		String str = s.nextLine();
+		String str = "Hello java";
+		
+		operation.charAt(str);
+		
+		operation.chars(str);
+		
+		operation.codePointAt(str);
+		
+		operation.codePointBefore(str);
+		
+		operation.codePointCount(str);
+		
+		operation.codePoints(str);
+		
+		operation.compareTo(str);
+		
+		operation.compareToIgnoreCase(str);
+		
+		operation.concat(str);
+		
+		operation.contains(str);
+		
+		operation.contentEquals(str);
+		
+		operation.describeConstable(str);
+		
+		operation.endsWith(str);
+		
+		operation.equalsIgnoreCase(str);
+		
+		operation.getBytes(str);
+		
+		operation.getBytes2(str);
+		
+		operation.hashCode(str);
+		
+		operation.indent(str);
+		
+		operation.indexOf(str);
+		
+		operation.indexOf1(str);
+		
+		operation.intern(str);
+		
+		operation.isBlank(str);
+		
+		operation.isEmpty(str);
+		
+		operation.lastIndexOf(str);
+		
+		operation.lastIndexOf1(str);
+		
+		operation.length(str);
+		
+		operation.repeat(str);
+		
+		operation.replace(str);
+		
+		operation.strip(str);
+		
+		operation.stripIndent(str);
+		
+		operation.stripLeading(str);
+		
+		operation.stripTrailing(str);
+		
+		operation.substring(str);
+		
+		operation.toLowerCase(str);
+		
+		operation.toUpperCase(str);
+		
+		operation.trim(str);
+		
+
+	}
+
+}
